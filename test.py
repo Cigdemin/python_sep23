29a7e6402044db89ee7e4f9dd332b332e45e19b6
cigdemin <cigmenci@hotmail.com>
2023-11-15 01:40:17 +0000

    Python notes updated

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 a = 15
 b = 4 
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a/b)
+print("This is the addition", a + b)
+print("This is the substraction",a-b)
+print("This is the multiplication",a*b)
+print("This is the divition",a/b)
