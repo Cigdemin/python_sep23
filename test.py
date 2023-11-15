138eee41a6a0bd619ee9d0872653f9c3e87d5903
cigdemin <cigmenci@hotmail.com>
2023-11-15 03:18:47 +0000

    updated

--- test.py
+++ test.py
@@ -1,6 +1,24 @@
-a = 15
-b = 4 
-print("This is the addition", a + b)
-print("This is the substraction",a-b)
-print("This is the multiplication",a*b)
-print("This is the divition",a/b)
+# a = 15
+# b = 4 
+# print("This is the addition", a + b)
+# print("This is the substraction",a-b)
+# print("This is the multiplication",a*b)
+# print("This is the divition",a/b)
+
+
+# string = "hello world"
+# print(string) #hello world
+# print(string[6]) #w
+# print(string[-1]) #d
+# print(string[0:5]) #hello
+# print(string[-5:]) #world
+# print(string[:6]) #hello 
+
+# text="The mesmerizing sunset painted the sky with hues of orange and pink, casting a warm glow over the-14:-6 tranquil beach."
+# print(text[4:15]) # Print the word - mesmerizing
+# print(text[-6:-1]) # Print the word - beach
+# print(text[-15:-6]) # Print the word - tranquil
+# print(text[16:]) # Print everything after word - mesmerizing   --> sunset painted the sky with hues of orange and pink, casting a warm glow over the tranquil beach.
+
+age=int(input("Please provide your age "))
+print("your age is", age , "nice to meet you", sep="||" , end=" ||||  This is the ending ")
