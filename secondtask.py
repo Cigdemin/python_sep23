f30eb8112d7e1fb81c858aeebb1c80072ec8b586
cigdemin <cigmenci@hotmail.com>
2023-11-15 02:12:33 +0000

    second task added

---
+++ secondtask.py
@@ -0,0 +1,7 @@
+name = input("Please enter your name: ")
+year = int(input("Please enter your age:" ))
+age = 2023 - year
+#Output:
+print("Hello", name)
+print(f"You are currently {age} years old.")
+print(f"Your birth year is: {year} ")
