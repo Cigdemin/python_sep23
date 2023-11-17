05ad0a060648f6a4cd47c23f0d085ef42558707c
cigdemin <cigmenci@hotmail.com>
2023-11-17 02:52:52 +0000

    leap year added

---
+++ leap_year.py
@@ -0,0 +1,5 @@
+year=int(input("Please enter a year: "))
+if (year % 100 != 0 and year % 4 == 0) and (year %4 == 0 ) :
+    print ("This is a leap year")
+else :
+    print ("This is not a leap year")
