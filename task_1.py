1b88e05b0d52e52215cc23ded4942bb561a42546
cigdemin <cigmenci@hotmail.com>
2023-11-22 03:13:23 +0000

    new files added

---
+++ task_1.py
@@ -0,0 +1,5 @@
+x=int(input("Please provide a number: "))
+d={}
+for i in range(x+1):
+    d[i] = i*i
+print(d)
---
+++ task_2.py
@@ -0,0 +1,17 @@
+sample_dict = {
+    'name1': 'John',
+    'name2': 'Alice',
+    'name3': 'Bob',
+    'name4': 'John',  # Duplicate value
+    'name5': 'Alice',  # Duplicate value
+    'name6': 'Max'
+}
+
+keys= {}
+values= {}
+for i in sample_dict :
+    keys { sample_dict.keys() }
+    values {sample_dict.values()}
+    print(keys)
+    print(values)
+
