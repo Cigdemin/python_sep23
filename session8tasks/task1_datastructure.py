c028ce87c80465b8d0ebfce12cd82d2de01c0247
cigdemin <cigmenci@hotmail.com>
2023-11-27 22:16:33 +0000

    session8 task1 and 2 added

---
+++ session8tasks/task1_datastructure.py
@@ -0,0 +1,14 @@
+# Create a data structure that will fit the following:
+# Kumo has a salary of 15000 and is 23 years old. He owns a few items such as a car, laptop, tv, and monitor. 
+# Mike is another person who makes 10000 and is 20 years old. He owns a car, PC, smartwatch, and boat
+
+kumo_dict = { "name" : "Kumo" , 
+"salary" : "15000" ,
+"age" : "23",
+"owned_items" : ["car" , "laptop" , "tv" ,"monitor" ]
+}
+mike_dict = { "name" : "Mike" , 
+"salary" : "10000" ,
+"age" : "20",
+"owned_items" : ["car" , "PC" , "smartwatch" ,"boat" ]
+}
---
+++ session8tasks/task2.py
@@ -0,0 +1,24 @@
+num_list = list(range(1,51))
+print(num_list)
+even_num = []
+odd_num = []
+for x in num_list:
+    if x % 2 == 0 : 
+        even_num.append(x)
+    else:
+        odd_num.append(x)
+print(even_num)
+print(odd_num)
+sum_even_num = 0
+for e in even_num:
+    sum_even_num += e
+print(f"Sum of all even numbers is: {sum_even_num}")
+product_of_odd = 1
+for o in odd_num:
+    product_of_odd *= o
+print(f"Product of all odd number is: {product_of_odd}")
+largest_num = 0
+for n in num_list:
+    if n > largest_num :
+        largest_num = n
+print(f"The largest num in the list is: {largest_num}")
