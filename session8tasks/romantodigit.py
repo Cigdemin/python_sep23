e0f6d0e033c73e6ceb173ff31e01c0159a975966
cigdemin <cigmenci@hotmail.com>
2023-12-01 00:09:01 +0000

    roman num added

--- session8tasks/romantodigit.py
+++ session8tasks/romantodigit.py
@@ -1,11 +1,18 @@
-dict = {'I':1, 'V':5,  'X':10, 'L':50,  'C':100,  'D':500,  'M':1000}
 # I can be placed before V (5) and X (10) to make 4 and 9. 
 # X can be placed before L (50) and C (100) to make 40 and 90. 
 # C can be placed before D (500) and M (1000) to make 400 and 900.
-rom_num = input("Please input a roman number: ")
-digit_num = 0
-for a in rom_num:
-	digit_num += dict[a]
-print(digit_num)
-# for n in dict:
-# 	print(dict[n])
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        s = (
+            s.replace("IV","IIII")
+            .replace("IX","VIIII")
+            .replace("XL","XXXX")
+            .replace("XC","LXXXX")
+            .replace("CD","CCCC")
+            .replace("CM","DCCCC")
+            )
+        digit_num = 0
+        for i in s:
+	        digit_num += dict[i]
+        return digit_num
